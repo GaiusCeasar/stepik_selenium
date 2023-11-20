3af3812fdda4b108f9a014147e0bfc4f07755a39
Maksim Ivahnenko <ivahnenkomv@anodialog.ru>
2023-11-20 17:27:49 +0300

    add

--- 1.2.py
+++ 1.2.py
@@ -1,32 +1,28 @@
-from webdrivermanager import ChromeDriverManager
-
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
+import time
 
-#driver = ChromeDriverManager()
-#driver.download_and_install()
-driver = webdriver.Chrome()
+link = "http://suninjuly.github.io/registration1.html"
 
-# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
-driver.get("https://suninjuly.github.io/text_input_task.html")
-time.sleep(5)
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
 
-# Метод find_element позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
-# Метод принимает в качестве аргументов способ поиска и значение, по которому мы будем искать
-# Ищем поле для ввода текста
-textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
+    input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Введите свое имя"]]').click()
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.XPATH, "//button[text()='Submit']")
+    button.click()
 
-# Напишем текст ответа в найденное поле
-textarea.send_keys("get()")
-time.sleep(5)
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
 
-# Найдем кнопку, которая отправляет введенное решение
-submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
-
-# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
-submit_button.click()
-time.sleep(5)
-
-# После выполнения всех действий мы должны не забыть закрыть окно браузера
-driver.quit()
+# не забываем оставить пустую строку в конце файла
