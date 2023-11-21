ec6b97581984cc3bfe7b40adfb553da42634c23a
Maksim Ivahnenko <ivahnenkomv@anodialog.ru>
2023-11-21 10:41:09 +0300

    add

---
+++ 2.1.5.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    answer = browser.find_element(By.CSS_SELECTOR, "#answer").send_keys(y)
+
+    robotCheckbox = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox").click()
+
+    robotsRule = browser.find_element(By.CSS_SELECTOR, "#robotsRule").click()
+
+    submit = browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
